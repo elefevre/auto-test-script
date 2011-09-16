7d191ea35b9523dc17b27a236878a2f6ef8b07a0
Eric Lefevre-Ardant <contact@ericlefevre.net>
2011-09-16 18:24:25 +0200

    Fixed finding the latest modified file

--- autotest.sh
+++ autotest.sh
@@ -4,11 +4,12 @@ TEST_COMMAND="$@"
 previousModifiedTime="0"
 
 while true; do
-	mostRecentlyChangedFile=`find . -not -type d -print  | sort -n | tail -1`
+	mostRecentlyChangedFile=`find . -mtime -2 -type f -exec ls -l {} \; | sort -k6 | tail -1 | awk '{print $9}'`
 	fileModifiedTime=`stat -f "%m" $mostRecentlyChangedFile`
 	if [ $previousModifiedTime != $fileModifiedTime ]; then
 		previousModifiedTime=$fileModifiedTime
 		clear
+		echo -e "Last run on \c"
 		date
 		$TEST_COMMAND 2>&1 | tail -1000		# replace with your own test command if needed
 	fi
