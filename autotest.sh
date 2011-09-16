f417c260fdfdc75f8473477bef2642745734b62d
Eric Lefevre-Ardant <contact@ericlefevre.net>
2011-09-16 15:16:56 +0200

    First version

---
+++ autotest.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+previousModifiedTime="0"
+
+while true; do
+	mostRecentlyChangedFile=`find . -not -type d -print  | sort -n | tail -1`
+	fileModifiedTime=`stat -f "%m" $mostRecentlyChangedFile`
+	if [ $previousModifiedTime != $fileModifiedTime ]; then
+		previousModifiedTime=$fileModifiedTime
+		clear
+		date
+		cake midje 2>&1 | tail -1000		# replace with your own test command if needed
+	fi
+	if [ $previousModifiedTime == $fileModifiedTime ]; then
+		echo -e "|\c"
+	fi
+	sleep 1
+done
